7245dbb72f54fe63f7bc2e0fb822f379f762372e
amitchackroborty04 <amitck803@gmail.com>
2026-06-27 11:22:36 +0600

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-  output: 'standalone',
+  output: "standalone",
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 
   images: {
     remotePatterns: [
